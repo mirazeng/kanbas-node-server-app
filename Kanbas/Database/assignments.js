69a7d456eae782ccd748b251c9bfad4aaa371647
mirazeng <zeng.wenqi@northeastern.edu>
2024-07-17 19:01:35 -0400

    a5 server

---
+++ Kanbas/Database/assignments.js
@@ -0,0 +1,110 @@
+export default [
+    {
+        "_id": "A101",
+        "title": "Propulsion Assignment",
+        "course": "RS101",
+        "available": "May 6 at 12:00am",
+        "due": "May 13 at 11:59pm",
+        "points": "100",
+        "due_date": "2019-05-13",
+        "available_from_date": "2019-05-06",
+        "until_date": "2019-05-13",
+        "description": "This assignment is available online. Submit a link to the landing page of your web application running on Netlify."
+    },
+    {
+        "_id": "A102",
+        "title": "Combustion Analysis",
+        "course": "RS101",
+        "available": "May 13 at 12:00am",
+        "due": "May 20 at 11:59pm",
+        "points": "100",
+        "due_date": "2019-05-20",
+        "available_from_date": "2019-05-13",
+        "until_date": "2019-05-20",
+        "description": "This assignment is available online. Submit a link to the landing page of your web application running on Netlify."
+    },
+    {
+        "_id": "A103",
+        "title": "Nozzle Design Project",
+        "course": "RS101",
+        "available": "May 20 at 12:00am",
+        "due": "May 27 at 11:59pm",
+        "points": "100",
+        "due_date": "2019-05-27",
+        "available_from_date": "2019-05-20",
+        "until_date": "2019-05-27",
+        "description": "This assignment is available online. Submit a link to the landing page of your web application running on Netlify."
+    },
+    {
+        "_id": "A201",
+        "title": "Aerodynamics Quiz",
+        "course": "RS102",
+        "available": "May 27 at 12:00am",
+        "due": "June 4 at 11:59pm",
+        "points": "100",
+        "due_date": "2019-06-04",
+        "available_from_date": "2019-05-27",
+        "until_date": "2019-06-04",
+        "description": "This assignment is available online. Submit a link to the landing page of your web application running on Netlify."
+    },
+    {
+        "_id": "A202",
+        "title": "Flow Analysis",
+        "course": "RS102",
+        "available": "Jun 4 at 12:00am",
+        "due": "Jun 11 at 11:59pm",
+        "points": "100",
+        "due_date": "2019-06-11",
+        "available_from_date": "2019-06-04",
+        "until_date": "2019-06-11",
+        "description": "This assignment is available online. Submit a link to the landing page of your web application running on Netlify."
+    },
+    {
+        "_id": "A203",
+        "title": "Heating Analysis",
+        "course": "RS102",
+        "available": "Jun 11 at 12:00am",
+        "due": "Jun 18 at 11:59pm",
+        "points": "100",
+        "due_date": "2019-06-18",
+        "available_from_date": "2019-06-11",
+        "until_date": "2019-06-18",
+        "description": "This assignment is available online. Submit a link to the landing page of your web application running on Netlify."
+    },
+    {
+        "_id": "A301",
+        "title": "Structural Design Task",
+        "course": "RS103",
+        "available": "Jun 18 at 12:00am",
+        "due": "Jun 25 at 11:59pm",
+        "points": "100",
+        "due_date": "2019-06-25",
+        "available_from_date": "2019-06-18",
+        "until_date": "2019-06-25",
+        "description": "This assignment is available online. Submit a link to the landing page of your web application running on Netlify."
+    },
+    {
+        "_id": "A302",
+        "title": "Orbital Calculations",
+        "course": "RS103",
+        "available": "Jun 25 at 12:00am",
+        "due": "July 2 at 11:59pm",
+        "points": "100",
+        "due_date": "2019-07-02",
+        "available_from_date": "2019-06-25",
+        "until_date": "2019-07-02",
+        "description": "This assignment is available online. Submit a link to the landing page of your web application running on Netlify."
+    },
+    {
+        "_id": "A303",
+        "title": "Systems Engineering Exam",
+        "course": "RS103",
+        "available": "July 2 at 12:00am",
+        "due": "July 9 at 11:59pm",
+        "points": "100",
+        "due_date": "2019-07-09",
+        "available_from_date": "2019-07-02",
+        "until_date": "2019-07-09",
+        "description": "This assignment is available online. Submit a link to the landing page of your web application running on Netlify."
+    }
+];
